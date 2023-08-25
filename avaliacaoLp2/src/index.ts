01476da29f3e402917aa57f07c33ea4181c84cf6
Robson-Higa <126790857+Robson-Higa@users.noreply.github.com>
2023-08-25 09:39:47 -0400

    Delete avaliacaoLp2 directory

--- avaliacaoLp2/src/index.ts
+++
@@ -1,9 +0,0 @@
-import { PositiveArray } from './models/PositiveArray'
-
-const array = new PositiveArray()
-array.add(1)
-array.add(10)
-array.add(-7)
-array.add(56)
-console.log(array.remove(1))
-console.log(array.values)
